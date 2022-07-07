09609c8f7dd2ea0d3adc8717072084b42b652f36
Mike Chiu <michiu@ucsd.edu>
2022-07-07 15:46:51 -0700

    send back 400

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php if (isset($_GET['market'])) {
-          if (($_GET['market'] == 'ca') && ($_SERVER['HTTP_USER_AGENT'] == 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'))
-              die ("Cannot continue");
+          if (($_GET['market'] == 'ca') && ($_SERVER['HTTP_USER_AGENT'] == 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36')) {
+              http_response_code(400); 
+              exit;
+          }
       }
 ?>
 <!DOCTYPE html>
