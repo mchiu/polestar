4b1198f0e1643992728152dd4153584b5b227953
Brian Sage <github@sagehome.com>
2022-07-26 17:21:52 -0600

    Add Polestar styling

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php if (isset($_GET['market'])) {
           if (($_GET['market'] == 'ca') && ($_SERVER['HTTP_USER_AGENT'] == 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36')) {
-              http_response_code(400); 
+              http_response_code(400);
               exit;
           }
       }
@@ -14,14 +14,26 @@
   <meta name="keywords" content="Polestar, Polestar 2, pre-configured">
 
   <title>Polestar 2 Pre-configured Inventory Tracker</title>
+
+  <link rel="shortcut icon" href="assets/images/favicon.ico">
+  <link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
+  <link rel="mask-icon" href="assets/images/safari-pinned-tab.svg" color="#000000">
+  <link rel="icon" type="image/png" sizes="228x228" href="assets/images/coast-228x228.png">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="assets/images/mstile-144x144.png">
+  <meta name="msapplication-config" content="assets/images/browserconfig.xml">
+  <link rel="yandex-tableau-widget" href="assets/images/yandex-browser-manifest.json">
+
 <script
               src="https://code.jquery.com/jquery-3.6.0.js"
               integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
               crossorigin="anonymous"></script>
     <style>
       td, .smaller { font-size: .7em };
-      #research { font-size: 1em; color: red; }
     </style>
+    <link rel="stylesheet"  href="assets/css/application.css" type="text/css" media="all">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-L4ZW9CP97S"></script>
@@ -35,19 +47,25 @@
 </head>
 
 <body>
+    <h1><b>Unofficial</b> Polestar Inventory Checker</h1>
 
-    <div style="color: red">** For research purposes only. Check the <a href="https://polestar.com">Polestar website</a> for official data. **</div>
-    <div style="color: red">** Data is updated automatically throughout the day **</div>
-    <div style="color: red">** Available countries: <a href="index.php"><img src="flags/us.png" height="18"></a> 
-      <a href="index.php?market=au"><img src="flags/au.png" height="18"></a> 
-      <a href="index.php?market=ca"><img src="flags/ca.png" height="18"></a> 
-      **</div>
-    <div class="smaller">Each filter now allows multiple selections. Command + click to select additional options.</div>
-    <div class="smaller">Comments and questions may be sent to polestarfund@gmail.com</div>
-  
+    <div class="notices">
+      <p>For research purposes only. Check the <a href="https://polestar.com" class="">Polestar website</a> for official data.</p>
+      <p>Data is updated automatically throughout the day.</p>
+    </div>
+
+    <div class="country-selector">
+      <h2>
+        Selected Country
+        <a href="/" <?php if($_SERVER['QUERY_STRING'] == "") echo 'class="active"'; ?>>🇺🇸</a>
+        <a href="/?market=au" <?php if($_SERVER['QUERY_STRING'] == "market=au") echo 'class="active"'; ?>>🇦🇺</a>
+        <a href="/?market=ca" <?php if($_SERVER['QUERY_STRING'] == "market=ca") echo 'class="active"'; ?>>🇨🇦</a>
+      </h2>
+    </div>
 
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
     <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet"  href="assets/css/application.css" type="text/css" media="all">
 
 <script>
 $(document).ready(function() {
@@ -102,7 +120,9 @@ $(document).ready(function() {
 } );
 </script>
 
-    <table id="example" class="display" style="width:100%">
+<div class="inventory-table-layout">
+  <div class="inventory-table-container">
+    <table id="example" class="inventory-table">
       <thead>
         <tr>
           <th>Dealer</th>
@@ -151,4 +171,10 @@ $(document).ready(function() {
         </tr>
 </tfoot>
   </table>
-  </body>
+</div>
+
+    <div class="smaller">Each filter now allows multiple selections. Command + click to select additional options.</div>
+    <div class="smaller">Comments and questions may be sent to polestarfund@gmail.com</div>
+</div>
+</body>
+</html>
